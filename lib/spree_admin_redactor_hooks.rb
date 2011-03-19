60c378130958d6d8821cc3e0886e5436c2215333
shaggyone <vzagorski@inbox.ru>
2011-03-19 23:05:27 +0700

    Now you can upload files directly from imperavi_redaktor

--- lib/spree_admin_redactor_hooks.rb
+++ lib/spree_admin_redactor_hooks.rb
@@ -1,3 +1,6 @@
 class SpreeAdminRedactorHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_inside_head, :partial => 'admin/shared/redactor_include'
+  insert_after :admin_tabs do
+    %(<%= tab(:imperavi_assets)%>)
+  end
 end
